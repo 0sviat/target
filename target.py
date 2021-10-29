7aa45b1e1f18c8759a3c503b83fc59f4947789df
0sviat <91615606+0sviat@users.noreply.github.com>
2021-10-29 13:59:23 +0300

    full_point_version

--- target.py
+++ target.py
@@ -1,9 +1,9 @@
-from typing import List
-
+"""
+TARGET GAME MODULE
+"""
 import random
 
-
-def generate_grid() -> List[List[str]]:
+def generate_grid():
     """
     Generates list of lists of letters - i.e. grid for the game.
     e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
@@ -13,11 +13,9 @@ def generate_grid() -> List[List[str]]:
     while letter_number < 9:
         if letter_number % 3 == 0:
             letters.append([])
-        letters[-1].append(chr(random.randint(65, 90)))
+        letters[-1].append(chr(random.randint(97, 122)))
         letter_number += 1
     return letters
-
-# print(generate_grid())
 
 
 def list_of_letter_tuples(word):
@@ -29,7 +27,7 @@ def list_of_letter_tuples(word):
     >>> list_of_letter_tuples('potato')
     [('a', 1), ('o', 2), ('p', 1), ('t', 2)]
     [('a', 1), ('e', 1), ('l', 1), ('p', 2)]
-    >>> list_of_letters_tuples('PyThOn')
+    >>> list_of_letter_tuples('PyThOn')
     [('h', 1), ('n', 1), ('o', 1), ('p', 1), ('t', 1), ('y', 1)]
     >>> list_of_letter_tuples('ERROR')
     [('e', 1), ('o', 1), ('r', 3)]
@@ -39,12 +37,47 @@ def list_of_letter_tuples(word):
     for ord_index in range(97, 123):
         # check by letter ACS if it is in a word
         if chr(ord_index) in word:
-            letters_list.append(((chr(ord_index), word.count(chr(ord_index)))))
+            letters_list.append((chr(ord_index), word.count(chr(ord_index))))
     return letters_list
-# print(list_of_letter_tuples('azAZ'))
 
 
-def get_words(file_with_words: str, letters: List[str]):
+def rule_correctness_word(word, letters):
+    """
+    (str, list) -> bool
+    Check if word fits game rules:
+    Word length in range [4,9]
+    Word includes central letter
+    All letters in the word are from grid board and itd
+    number of occurrences in words is not more than in the grid board
+    """
+    central_letter = letters[4]
+    # if word is too short or too long or do not include central letter
+    if len(word) < 4 or len(word) > 9 or central_letter not in word:
+        return False
+    # create list[tuples] with letters in alphabet order and their number of occurrences in grid
+    grid_letters = list_of_letter_tuples(' '.join(letters))
+    # create list[tuple] with letters in alphabet order and their number of occurrences in word
+    word_letters = list_of_letter_tuples(word)
+
+    starting_index_in_grid_letters, index_in_word = 0, 0
+    # check letters in the word one by one
+    while index_in_word < len(word_letters):
+        # check if current letter from a word is in grid
+        for i in range(starting_index_in_grid_letters, len(grid_letters) + 1):
+            # if we have checked all letters from grid but there still are letters in word
+            if i == len(grid_letters):
+                return False
+            if word_letters[index_in_word][0] == grid_letters[i][0]:
+                # then check letter's number of occurrences in the word
+                if word_letters[index_in_word][1] <= grid_letters[i][1]:
+                    starting_index_in_grid_letters = i + 1
+                    # change starting letter to next by the alphabets in grid letters
+                    break
+                return False
+        index_in_word += 1  # change letter in word_letter_list to next one
+    return True
+
+def get_words(file_with_words: str, letters):
     """
     Reads the file f. Checks the words with rules and returns a list of words.
     """
@@ -52,88 +85,56 @@ def get_words(file_with_words: str, letters: List[str]):
     # open file with words and read it line by line
     with open(file_with_words, mode='r', encoding='utf-8') as dictionary:
         for word in dictionary:
-            # read new word from the file
-            current_word = word.read()
+            # read new word from the file with no symbol new line and in lower format
+            current_word = word.strip().lower()
             # if a word corresponds to the rules add it to the list
-            if rule_correctness_word(current_word,letters):
+            if rule_correctness_word(current_word, letters):
                 words_from_dictionary.append(current_word)
 
     return words_from_dictionary
 
-
-def rule_correctness_word(word, letters):
-    """
-    (str, list) -> bool
-    Check if word fits game rules:
-    Word lenght in range [4,9]
-    Word includes central letter
-    All letters in the word are from grid board and itd
-    number of occurrences in words is not more than in the grid board
-    """
-    central_letter = letters[5]
-    # if word is too short or too long or do not include central letter
-    if len(word) < 4 or len(word) > 9 or central_letter not in word:
-        return False
-    # create list[tuples] with letters in alphabet order and their number of occurrences in grid
-    grid_letters = list_of_letter_tuples(''.join(letters))
-    # create list[tuple] with letters in alphabet order and their number of occurrences in word
-    word_letters = list_of_letter_tuples(word)
-
-    startig_index_in_grid_letters, index_in_word = 0, 0
-    # check letters in the word one by one
-    while index_in_word < len(word_letters):
-        # check if current letter from a word is in grid
-        for i in range(startig_index_in_grid_letters, len(grid_letters)):
-            if word_letters[index_in_word][0] == grid_letters[i][0]:
-                # then check letter's number of occurrences in the word
-                if word_letters[index_in_word][1] <= grid_letters[i][1]:
-                    # change starting letter to next by the alphabetos in grid letters
-                    startig_index_in_grid_letters = i+1
-                else:
-                    return False
-        index_in_word += 1   # change letter in word_letter_list to next one
-    return True
-
-
-def get_user_words() -> List[str]:
+def get_user_words():
     """
     Gets words from user input and returns a list with these words.
     Usage: enter a word or press ctrl+d to finish.
     """
-    list_of_player_worlds = []
-    while True:
-        player_world = input()
-        if player_world == '^D':
-            break
-        list_of_player_worlds.append(player_world)
+    list_of_player_worlds = input().split()
     return list_of_player_worlds
 
 
-def get_pure_user_words(user_words: List[str], letters: List[str], words_from_dict: List[str]) -> List[str]:
+def get_pure_user_words(user_words, letters, words_from_dict):
     """
     (list, list, list) -> list
     Checks user words with the rules and returns list of those words
     that are not in dictionary.
     """
     wrong_words = []
-    not_in_dict_words =[]
     for word in user_words:
-        if  rule_correctness_word(word, letters) and word not in words_from_dict:
-            wrong_words.append(not_in_dict_words)
+        if rule_correctness_word(word, letters) and (word not in words_from_dict):
+            wrong_words.append(word)
     return wrong_words
 
+
 def results():
+    """
+    Return results of the game
+    """
     letters = generate_grid()
     letters = letters[0] + letters[1] + letters[2]
     right_words_number = 0
     dict_words = get_words('en', letters)
     user_words = get_user_words()
-    wrong_words = get_pure_user_words(user_words, letters,dict_words)
+    wrong_words = get_pure_user_words(user_words, letters, dict_words)
 
     for one_user_word in user_words:
-        if  one_user_word  in dict_words:
-            right_words_number+=1
+        if one_user_word in dict_words:
+            right_words_number += 1
             dict_words.remove(one_user_word)
 
-    result = right_words_number + dict_words + wrong_words
-    return result
+    print(right_words_number)
+    print(dict_words)
+    print(wrong_words)
+    with open ('results.txt', 'w', encoding='utf-8') as result_text:
+        result_text.write(str(right_words_number)+'\n')
+        result_text.write('\n'.join(dict_words))
+        result_text.write('\n'.join(wrong_words))
